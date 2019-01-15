d903c04594754a20aa14ff22277cf64e9c0436c6
Curtis M <34104460+CurtisMac@users.noreply.github.com>
2019-01-15 14:46:23 -0800

    starts styling header and children
    this commit saves only partial styling of the header and header navigation. created to save styles before refactoring file structure.

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -1,20 +1,32 @@
 import React from "react";
 import styled from "styled-components";
 
-//Components
-import Nav from "./_headerNav";
-
-const StyledHeader = styled.header`
-  background-color: ${props => props.theme.purpleDark};
-`;
+//components
+import HeaderNav from "./HeaderNav";
 
 const Header = () => {
   return (
     <StyledHeader>
-      <h1>this is a header</h1>
-      <Nav />
+      <Title>LOGO</Title>
+      <NavContainer />
     </StyledHeader>
   );
 };
+
+//styles
+const StyledHeader = styled.header`
+  background: transparent;
+  display: flex;
+`;
+
+const Title = styled.h1`
+  padding: 10px;
+  margin: 0 0 20px 0;
+  color: coral;
+`;
+
+const NavContainer = styled(HeaderNav)`
+  /* flex styles */
+`;
 
 export default Header;
---
+++ components/layout/HeaderNav.js
@@ -0,0 +1,30 @@
+import Link from "next/link";
+import styled from "styled-components";
+
+//components
+import StyledButton from "../styles/StyledButton";
+
+class Nav extends React.Component {
+  render() {
+    return (
+      <StyledNav className={this.props.className}>
+        <Link href="/">
+          <StyledButton bgColor={"green"}>Login</StyledButton>
+        </Link>
+        <Link href="/">
+          <StyledButton bgColor={"blue"} solid>
+            Signup
+          </StyledButton>
+        </Link>
+      </StyledNav>
+    );
+  }
+}
+
+//styles
+const StyledNav = styled.nav`
+  color: red;
+  font-size: 1.3rem;
+`;
+
+export default Nav;
