628e6ca59e3a8f654be7cc6dc0f5713eb2a97e26
Curtis M <34104460+CurtisMac@users.noreply.github.com>
2019-01-15 21:50:32 -0800

    add anchor link to header logo

--- components/header/Logo.js
+++ components/header/Logo.js
@@ -1,16 +1,20 @@
-import React from "react";
+import Link from "next/link";
 import styled from "styled-components";
 
 import { media } from "../styles/style-utils";
 
-const Logo = () => {
-  return <Title>Squidlee</Title>;
-};
-
+const Logo = () => (
+  <div>
+    <Link href="/">
+      <Title>Squidlee</Title>
+    </Link>
+  </div>
+);
 const Title = styled.h1`
   padding: 2px;
   margin: 10px 10px 5px;
   color: coral;
+  cursor: pointer;
   ${media.phone`
     color: green;
   `}
