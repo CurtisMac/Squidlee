4347b032b36f47a70f0d3d1fd9c132394c97cadb
Curtis M <34104460+CurtisMac@users.noreply.github.com>
2019-01-16 13:35:32 -0800

    updated color scheme

--- components/styles/Themes.js
+++ components/styles/Themes.js
@@ -1,9 +1,36 @@
 const MainTheme = {
+  //Primary colors
+  primaryLighter: "#8952CB",
+  primaryLight: "#6D449F",
+  primary: "#631ABB",
+  primaryDark: "#4C2080",
+  primaryDarker: "#400F7A",
+  //Secondary colors
+  secondaryLighter: "#DA4DA9",
+  secondaryLight: "#B5448D",
+  secondary: "#CF0D8B",
+  secondaryDark: "#921A68",
+  secondaryDarker: "#8B075D",
+  //Tetiary colors
+  tetiaryLighter: "#5969CC",
+  tetiaryLight: "#4955A1",
+  tetiary: "#2338BD",
+  tetiaryDark: "#253282",
+  tetiaryDarker: "#15237B",
+  //Complementary colors
+  complementLighter: "#FFF25A",
+  complementLight: "#EADF58",
+  complement: "#FFEC10",
+  complementDark: "#BEB122",
+  complementDarker: "#B4A609"
+};
+
+export { MainTheme };
+
+const old = {
   primaryLighter: "#9576AB",
   primaryLight: "#734C8F",
   primary: "#542B72",
   primaryDark: "#3A1356",
   primaryDarker: "#230439"
 };
-
-export { MainTheme };
