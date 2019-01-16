5afc2e2b3dcac0e77d425bf85dd1ae67b167f827
Curtis M <34104460+CurtisMac@users.noreply.github.com>
2019-01-15 21:13:34 -0800

    added media query util

--- components/header/Logo.js
+++ components/header/Logo.js
@@ -1,6 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 
+import { media } from "../styles/style-utils";
+
 const Logo = () => {
   return <Title>Squidlee</Title>;
 };
@@ -9,6 +11,9 @@ const Title = styled.h1`
   padding: 2px;
   margin: 10px 10px 5px;
   color: coral;
+  ${media.phone`
+    color: green;
+  `}
 `;
 
 export default Logo;
--- components/styles/style-utils.js
+++ components/styles/style-utils.js
@@ -1,11 +1,20 @@
 import { css } from "styled-components";
 
-const media = {
-  phone: (...args) => css`
-    @media (max-width: 420px) {
+const screenSizes = {
+  giant: 1170,
+  desktop: 992,
+  tablet: 768,
+  phone: 420
+};
+
+const media = Object.keys(screenSizes).reduce((a, label) => {
+  const emSize = screenSizes[label] / 16;
+  a[label] = (...args) => css`
+    @media (max-width: ${emSize}em) {
       ${css(...args)}
     }
-  `
-};
+  `;
+  return a;
+}, {});
 
 export { media };
