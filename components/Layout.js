671a705a4734755988af9026255b9d1da141a564
Curtis M <34104460+CurtisMac@users.noreply.github.com>
2019-01-16 22:07:43 -0800

    added component to Layout.js to make main section 100vh

--- components/Layout.js
+++ components/Layout.js
@@ -1,16 +1,21 @@
 import { default as Head } from "./Meta";
 import Header from "./header";
 import Footer from "./footer";
+import styled from "styled-components";
 
 const Layout = ({ children }) => {
   return (
     <div>
       <Head />
       <Header />
-      {children}
+      <Main>{children}</Main>
       <Footer />
     </div>
   );
 };
+
+const Main = styled.main`
+  height: 100vh;
+`;
 
 export default Layout;
