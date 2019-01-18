27c5445d95edb341822e7d362c91222fdfb05eec
Curtis M <34104460+CurtisMac@users.noreply.github.com>
2019-01-18 09:33:46 -0800

    refactored pages/index.js to bypass Layout.js

--- components/Meta.js
+++ components/Meta.js
@@ -1,8 +1,8 @@
 import Head from "next/head";
 
-const Meta = () => (
+const Meta = props => (
   <Head>
-    <title>Squidlee</title>
+    <title>{props.title}</title>
     <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     <meta charSet="utf-8" />
     <link rel="shortcut icon" href="/static/favicon.ico" />
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,28 @@
 import styled from "styled-components";
 import Layout from "../components/Layout";
 
+import { default as Head } from "../components/Meta";
+import Header from "../components/header/index";
+import Footer from "../components/footer/index";
+
 const Home = () => {
   return (
-    <Layout>
-      <StyledH1>Hi! This is the home page</StyledH1>
-    </Layout>
+    <React.Fragment>
+      <Head title={"Squidlee"} />
+      <Header />
+      <Main>
+        <StyledH1>Hi! This is the home page</StyledH1>
+      </Main>
+      <Footer />
+    </React.Fragment>
   );
 };
 
 const StyledH1 = styled.h1`
   color: ${props => props.theme.textBody};
+`;
+const Main = styled.main`
+  height: 80vh;
 `;
 
 export default Home;
