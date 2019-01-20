1145119b43dbd5f78bf813a96ce7aaec6ea772ef
Curtis M <34104460+CurtisMac@users.noreply.github.com>
2019-01-19 18:06:18 -0800

    initialized formik

--- pages/signup.js
+++ pages/signup.js
@@ -1,7 +1,24 @@
 import styled from "styled-components";
+import { default as Head } from "../components/Meta";
+import Header from "../components/header/index";
+import SignupForm from "../components/signupForm/index";
 
 const signup = () => {
-  return <div>This is the signup page</div>;
+  return (
+    <React.Fragment>
+      <Head title={"Signup"} />
+      <Container>
+        <Header noNav />
+        <main>
+          <SignupForm />
+        </main>
+      </Container>
+    </React.Fragment>
+  );
 };
+
+const Container = styled.div`
+  min-height: 100vh;
+`;
 
 export default signup;
