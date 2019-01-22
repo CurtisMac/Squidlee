b8e112122a0e9f94801fdb599f1e9a182d715af3
Curtis M <34104460+CurtisMac@users.noreply.github.com>
2019-01-22 08:30:09 -0800

    reformatted StyledButton to take color as string

--- components/styles/StyledButton.js
+++ components/styles/StyledButton.js
@@ -2,14 +2,17 @@ import styled from "styled-components";
 
 const StyledButton = styled.button`
   background: ${props =>
-    props.solid ? props.bgColor || props.theme.primary : "transparent"};
+    props.solid
+      ? props.theme[props.bgColor] || props.theme.primary
+      : "transparent"};
   color: ${props =>
     props.solid
       ? props.txtColor || "white"
       : props.txtColor
       ? props.txtColor
-      : props.bgColor || props.theme.primary};
-  border: 2px solid ${props => props.bgColor || props.theme.primary};
+      : props.theme[props.bgColor] || props.theme.primary};
+  border: 2px solid
+    ${props => props.theme[props.bgColor] || props.theme.primary};
   border-radius: 5px;
   margin: 0.5rem;
   padding: 0.25em 1em;
