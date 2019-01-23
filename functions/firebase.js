f4e3f6923ca95c5c2c4ecf6232bbb174e8fa70b6
Curtis M <34104460+CurtisMac@users.noreply.github.com>
2019-01-22 16:14:49 -0800

    added firebase init files

---
+++ functions/firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase";
+
+const config = {
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  databaseURL: process.env.databaseURL,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId
+};
+
+const firebaseApp = firebase.initializeApp(config);
+
+export default firebaseApp;
+q;
